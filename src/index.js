7dd7c02c1839bfad6654287d7029fea75c00f309
Natallia Yurcha <natallia.yurcha@gmail.com>
2021-03-15 11:59:19 +0300

    feat: implement reverse-int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let num = 0;
+    let result = 0;
+    n = Math.abs(n);
+    while (n > 0) {
+        num = n % 10;
+        result = result * 10 + num;
+        n = Math.floor(n / 10);
+    }
+    return result;
 }
